05562991d62f0602270370be3900c6c7f0a1e21e
DgDevHub <cavaleirooficia@gmail.com>
2025-06-04 16:21:39 -0300

    :broom: cleanup: Limpeza de arquivos que não estavam sendo usados

--- src/app/sections/Contato/Contato.tsx
+++ src/app/sections/Contato/Contato.tsx
@@ -13,7 +13,7 @@ export default function Contato() {
          <Divider hasBackground="var(--black)" colors="var(--title-colorTwo)" h2="Contato" p="Onde me contratar" />
 
         <div className={style.container}>
-        <Icon link="" icon="/Images/email.png" title="Email" p="Diego13112019@outlook.com"/>
+        <Icon link="" icon="/Images/email.png" title="Email" p="diegosilvacarvalhodev@gmail.com"/>
 
         <Icon link="https://www.twitch.tv/devedg/" icon="/Images/twitch.png" title="Twitch" p="DeveDg"/>
 
